3a1471672ae7908b9b9ff7bbbbe1e47303dec295
henrique-magana <henriquesmagana@gmail.com>
2023-09-19 23:24:40 -0300

    fazendo mais uma parte

---
+++ 1 - Tipos, Variaveis e Funcoes/conversoes.js
@@ -0,0 +1,7 @@
+const numero = 456
+
+const string = "456"
+
+
+console.log(numero + Number(string))
+
---
+++ 1 - Tipos, Variaveis e Funcoes/truthy_false.js
@@ -0,0 +1,14 @@
+const n1 = true
+const n2 = false
+
+console.log(0 == false);
+console.log("" == false)
+console.log(1 == true)
+
+// null -  representa o vazio
+
+let n = 8
+let m = "eaee";
+console.log(typeof(n))
+console.log(typeof(m))
+
